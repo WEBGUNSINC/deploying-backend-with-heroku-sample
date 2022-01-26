c282d5fb1143e66499c9e7e30b3af934efe79c1f
AntonioR <antonio.russo@yonkersnet.com>
2022-01-26 11:57:49 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const myHeaders = new Headers();
 myHeaders.append('Authorization', 'bearer AuXYdS7BiIkWTi1xiQfS6pbb2PUC47b2yEx');
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
-  fetch('https://api.turnto.com/v1.2/reviews?sku=01_1020_04&includeRelated=false', {
-    method: 'GET',
-    headers: myHeaders,
-}) // fetch TT reviews
+  fetch('https://api.turnto.com/v1.2/reviews?sku=01_1020_04&includeRelated=false') // fetch TT reviews
     .then(res => res.json()) // return a promise containing the response
     .then(json => res.send(`<h1> ${json.title}!</h1>`)) // extract the JSON body content from the response (specifically the title value) and sends it to the client
     .catch(function(err){ // catch any errors
@@ -22,3 +19,8 @@ app.listen(PORT, () => { // start server and listen on specified port
   console.log(`App is running on ${PORT}`) // confirm server is running and log port to the console
 }) 
 
+
+// , {
+//     method: 'GET',
+//     headers: myHeaders,
+// }
