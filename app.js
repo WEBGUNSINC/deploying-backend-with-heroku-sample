5f22a0e1f7661cebe13ffd6e96b0a70d43f88b51
AntonioR <antonio.russo@yonkersnet.com>
2022-01-26 11:55:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express(); // create an instance of the express module (app is the c
 const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
+const myHeaders = new Headers();
+myHeaders.append('Authorization', 'bearer AuXYdS7BiIkWTi1xiQfS6pbb2PUC47b2yEx');
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
   fetch('https://api.turnto.com/v1.2/reviews?sku=01_1020_04&includeRelated=false', {
     method: 'GET',
-    headers: {
-              'Authorization', 'bearer AuXYdS7BiIkWTi1xiQfS6pbb2PUC47b2yEx',
-              },
-      }) // fetch TT reviews
+    headers: myHeaders,
+}) // fetch TT reviews
     .then(res => res.json()) // return a promise containing the response
     .then(json => res.send(`<h1> ${json.title}!</h1>`)) // extract the JSON body content from the response (specifically the title value) and sends it to the client
     .catch(function(err){ // catch any errors
