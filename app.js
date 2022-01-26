eab304aa0396769494af0f02e2eeacd48d645708
AntonioR <antonio.russo@yonkersnet.com>
2022-01-26 16:48:39 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express'); // import express module (simplifies routing/requests, among other things)
-const path = require('path');
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
 const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
@@ -8,7 +7,7 @@ const ttapitok = process.env.ttbearer
 var sku = req.query.sku;
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
-  fetch('https://api.turnto.com/v1.2/reviews?sku='+sku, {
+  fetch('https://api.turnto.com/v1.2/reviews?sku=Z94__194', {
     method: 'GET',
     headers: { 'Authorization': 'bearer ' + ttapitok}
   }) // fetch TT reviews
